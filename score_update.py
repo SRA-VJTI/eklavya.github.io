51d36f729984fa4413e4996cb46fd753e6e98ebe
kkothari2001 <kkothari2001@users.noreply.github.com>
2021-08-09 04:52:42 +0000

    deploy: d09bbf513f95e6f311ecd4261bc06baa75d04fda

--- score_update.py
+++ score_update.py
@@ -1,42 +1,29 @@
 from pprint import pprint
+import sys
 
-file1 = open('./scoreboard.md', 'r')
+
+file1 = open('./content/scoreboard.md', 'r')
 
 lines = [line.split("|") for line in file1.readlines()]
 file1.close()
-team_list = []
+team_dict = {}
 i = 0
 
 for line in lines:
     if i == 0 or i == 1:
         i += 1
         continue
-    team = []
-    team.append(line[1].strip())
-    team.append(int(line[2].strip()))
-    team_list.append(team)
+    team_dict[line[1]] = int(line[2])
     i += 1
 
-i = 1
-for team in team_list:
-    print("{}. {}".format(i, team[0]))
-    i += 1
+input_string = sys.argv[1]
+team_name = input_string.split(":")[0]
+team_score_change = int(input_string.split(":")[1])
+team_dict[team_name] += team_score_change
 
+team_dict = {k: v for k, v in sorted(team_dict.items(), key=lambda item: item[1], reverse=True)}
 
-team_change = int(input("Print team name to update:"))
-team_change -= 1
-# print(team_list[team_change][0])
-points_change = int(input("Add marks: "))
-team_list[team_change][1] += points_change
-
-
-def cmp(x):
-    return x[1]
-
-
-team_list.sort(key=cmp, reverse=True)
-
-with open("scoreboard.md", "w") as scoreb:
+with open("./content/scoreboard.md", "w") as scoreb:
     scoreb.write("|Team|Score|\n|---|---|\n")
-    for team in team_list:
-        scoreb.write("|{}|{}|\n".format(team[0], team[1]))
+    for team, score in team_dict.items():
+        scoreb.write("|{}|{}|\n".format(team, score))
