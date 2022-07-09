22f6aec013f2b8a29eb770c96b399e729df3a5fc
Dhruv454000 <Dhruv454000@users.noreply.github.com>
2022-07-09 06:32:14 +0000

    deploy: ffa8cc82bc08d591652156dbf1502ad9ff0ba673

--- score_update.py
+++ score_update.py
@@ -2,7 +2,7 @@ from pprint import pprint
 import sys
 
 
-file1 = open('./content/scoreboard.md', 'r')
+file1 = open('./scoreboard.md', 'r')
 
 lines = [line.split("|") for line in file1.readlines()]
 file1.close()
@@ -15,15 +15,16 @@ for line in lines:
         continue
     team_dict[line[1]] = int(line[2])
     i += 1
+for teams in range(1,len(sys.argv)):
+    input_string = sys.argv[teams]
+    team_name = input_string.split(":")[0]
+    team_score_change = int(input_string.split(":")[1])
+    team_dict[team_name] += team_score_change
 
-input_string = sys.argv[1]
-team_name = input_string.split(":")[0]
-team_score_change = int(input_string.split(":")[1])
-team_dict[team_name] += team_score_change
+    team_dict = {k: v for k, v in sorted(team_dict.items(), key=lambda item: item[1], reverse=True)}
 
-team_dict = {k: v for k, v in sorted(team_dict.items(), key=lambda item: item[1], reverse=True)}
-
-with open("./content/scoreboard.md", "w") as scoreb:
-    scoreb.write("|Team|Score|\n|---|---|\n")
-    for team, score in team_dict.items():
-        scoreb.write("|{}|{}|\n".format(team, score))
+    with open("./scoreboard.md", "w") as scoreb:
+        scoreb.write("|Team|Score|\n|---|---|\n")
+        for team, score in team_dict.items():
+            scoreb.write("|{}|{}|\n".format(team, score))
+            print(team,score)
