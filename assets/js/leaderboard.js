9f0872b6f081aad6e252d690935a330355e87954
SuperChamp234 <beingzainsv@gmail.com>
2023-06-28 06:26:47 +0000

    Deploying to gh-pages from @ SRA-VJTI/eklavya.github.io@dd01a72343ed8a9ec2e3b919dee3b6c738313961 🚀

--- assets/js/leaderboard.js
+++ assets/js/leaderboard.js
@@ -1,6 +1,6 @@
 let SHEET_ID = "1K9QtML3lSrBIk0UNDydKRtBL3TNEnZO5VyU08mqwE1Q";
 let SHEET_TITLE = "Eklavya Leaderboard";
-let SHEET_RANGE = "A2:B9";
+let SHEET_RANGE = "A2:B42";
 
 let full_URI = `https://docs.google.com/spreadsheets/d/${SHEET_ID}/gviz/tq?sheet=${SHEET_TITLE}/&range=${SHEET_RANGE}`;
 
@@ -8,18 +8,21 @@ fetch(full_URI)
   .then((res) => res.text())
   .then((rep) => {
     let data = JSON.parse(rep.substr(47).slice(0, -2));
-    let leaderboardData = {};
+    let leaderboardData = [];
 
     for (row in data.table.rows) {
       let teamName = data.table.rows[row]["c"][0]["v"];
-      let Score = data.table.rows[row]["c"][1]["f"];
-      leaderboardData[teamName] = Score;
+      let score = data.table.rows[row]["c"][1]["f"];
+      leaderboardData.push({ teamName, score });
     }
+
+    // Sort the leaderboardData array based on the score
+    leaderboardData.sort((a, b) => b.score - a.score);
 
     let leaderboardBody = document.getElementById("leaderboardBody");
     leaderboardBody.innerHTML = ""; // Clear existing content
 
-    if (Object.keys(leaderboardData).length === 0) {
+    if (leaderboardData.length === 0) {
       // If leaderboardData is empty, display "No matching record found" message
       let row = document.createElement("tr");
       let cell = document.createElement("td");
@@ -29,7 +32,7 @@ fetch(full_URI)
       leaderboardBody.appendChild(row);
     } else {
       let count = 1;
-      for (let key in leaderboardData) {
+      for (let i = 0; i < leaderboardData.length; i++) {
         let row = document.createElement("tr");
         let idCell = document.createElement("th");
         idCell.setAttribute("scope", "row");
@@ -37,11 +40,11 @@ fetch(full_URI)
         row.appendChild(idCell);
 
         let firstCell = document.createElement("td");
-        firstCell.textContent = key;
+        firstCell.textContent = leaderboardData[i].teamName;
         row.appendChild(firstCell);
 
         let lastCell = document.createElement("td");
-        lastCell.textContent = leaderboardData[key];
+        lastCell.textContent = leaderboardData[i].score;
         row.appendChild(lastCell);
 
         leaderboardBody.appendChild(row);
